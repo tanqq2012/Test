1f866856e94509fd386fb1f4547a1785c30e6f40
tanqq2002 <tanqq2002@hotmail.com>
2014-02-27 23:45:14 +0800

    ceshi

---
+++ ef/ef/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.Practices.Unity;
+
+namespace ef
+{
+    interface ITest
+    {
+        void Test();
+    }
+
+    class TestA : ITest
+    {
+        public void Test() { Console.WriteLine("A"); }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var container = new UnityContainer();
+
+         
+
+
+
+
+
+
+           
+        }
+    }
+}
